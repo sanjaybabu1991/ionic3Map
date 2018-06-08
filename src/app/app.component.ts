7b4f16710a7bebee0d255b14381d09cfaa0ea5a6
sanjay91 <sbabu@iiserb.ac.in>
2018-06-08 16:28:31 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,16 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+//firebase
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'YOUR_APIKEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  databaseURL: 'YOUR_DATABASE_URL',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+};
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -17,6 +27,7 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+declare var google: any;
+
+console.log();
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,60 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  markers = [];
+  constructor(public navCtrl: NavController, public platform: Platform, private geolocation: Geolocation) {
 
+    
+      platform.ready().then(() => {
+        this.initMap();
+      });
+  }
+
+  initMap() {
+    this.geolocation.getCurrentPosition({ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }).then((resp) => {
+      let mylocation = new google.maps.LatLng(resp.coords.latitude,resp.coords.longitude);
+      this.map = new google.maps.Map(this.mapElement.nativeElement, {
+        zoom: 15,
+        center: mylocation
+      });
+    });
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      this.deleteMarkers();
+      let updatelocation = new google.maps.LatLng(data.coords.latitude,data.coords.longitude);
+      let image = 'assets/imgs/sanjay.png';
+      this.addMarker(updatelocation,image);
+      this.setMapOnAll(this.map);
+    });
+  }
+
+  addMarker(location, image) {
+    let marker = new google.maps.Marker({
+      position: location,
+      map: this.map,
+      icon: image
+    });
+    this.markers.push(marker);
+  }
+  
+  setMapOnAll(map) {
+    for (var i = 0; i < this.markers.length; i++) {
+      this.markers[i].setMap(map);
+    }
+  }
+  
+  clearMarkers() {
+    this.setMapOnAll(null);
+  }
+  
+  deleteMarkers() {
+    this.clearMarkers();
+    this.markers = [];
   }
 
 }
+
+
+
